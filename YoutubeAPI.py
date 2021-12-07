2be26501d85f4202bcbbe898f26d1282239430fd
manueljamorim <manueljoaoamorim@gmail.com>
2021-12-07 19:02:10 +0000

    correcao erro sql caso esteja a haver

--- YoutubeAPI.py
+++ YoutubeAPI.py
@@ -1,38 +1,37 @@
 from apiclient.discovery import build
 from apiclient.errors import HttpError
 
+#pip install apiclient
+
 DEVELOPER_KEY = "AIzaSyDSLv-JDuqmg22HQIClcGiEZkJsiW5rO6o"
 YOUTUBE_API_SERVICE_NAME = "youtube"
 YOUTUBE_API_VERSION = "v3"
 
 def youtube_search(titulo):
-  youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
+    youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
     developerKey=DEVELOPER_KEY)
 
-  # Call the search.list meqthod to retrieve results matching the specified
-  # query term.
-  search_response = youtube.search().list(
-    q=titulo,
-    part="id,snippet",
-    maxResults=1
-  ).execute()
+    # Call the search.list meqthod to retrieve results matching the specified
+    # query term.
+    search_response = youtube.search().list(
+        q=titulo,
+        part="id,snippet",
+        maxResults=1
+    ).execute()
 
-  videos = []
+    videos = []
 
-  # Add each result to the appropriate list, and then display the lists of
-  # matching videos, channels, and playlists.
-  for search_result in search_response.get("items", []):
-    if search_result["id"]["kind"] == "youtube#video":
-      videos.append("%s (%s)" % (search_result["snippet"]["title"],
-                                 search_result["id"]["videoId"]))
+    # Add each result to the appropriate list, and then display the lists of
+    # matching videos, channels, and playlists.
+    for search_result in search_response.get("items", []):
+        if search_result["id"]["kind"] == "youtube#video":
+            videos.append("%s (%s)" % (search_result["id"]["videoId"]))
+            
+    return videos[0]
+  
+  
+    
+    
 
 
-  print("Videos:\n", "\n".join(videos), "\n")
 
-lista_musicas = ["fuck forex"]
-
-for i in lista_musicas:
-  try:
-    youtube_search(i)
-  except HttpError as e:
-    print("An HTTP error %d occurred:\n%s" % (e.resp.status, e.content))
--- population.py
+++ population.py
@@ -1,157 +1,161 @@
 from urllib.request import urlopen
 import json
 import random
-#import names #tem de se instalar cmd -> sudo pip install names 
-
-#Utilizacao do DEEZER API para importar dados
-
-#TO DO LIST
-#Utilizador, playlist, banda, artista, concerto, videoclip, ator, produtor,
-#VideoclipAtor, VideoclipProdutor, ConcertoAutor, ConcertoMusica,
-#GenerosFavoritos, BandaArtista, AutorBandaArtista
 
 
-#Table: Album
+import os, ssl
+if (not os.environ.get('PYTHONHTTPSVERIFY', '') and getattr(ssl, '_create_unverified_context', None)):
+    ssl._create_default_https_context = ssl._create_unverified_context
+# import names #tem de se instalar cmd -> sudo pip install names
+
+
+# Utilizacao do DEEZER API para importar dados
+
+# TO DO LIST
+# Utilizador, playlist, banda, artista, concerto, videoclip, ator, produtor,
+# VideoclipAtor, VideoclipProdutor, ConcertoAutor, ConcertoMusica,
+# GenerosFavoritos, BandaArtista, AutorBandaArtista
+
+
+# Table: Album
 albums_list = []
-#(id nome  dataLancamento idEditora)
+# (id nome  dataLancamento idEditora)
 
-#Table: Editora (swap order)
+# Table: Editora (swap order)
 editora_key = {}
 
-#Table: Musica
+# Table: Musica
 music_list = []
-#(id nome  imagemcapa reproducoes idvideoclip)
-#!! falta id vioclip
-#reproducoes ->random generator
+# (id nome  imagemcapa reproducoes idvideoclip)
+# !! falta id vioclip
+# reproducoes ->random generator
 
-#Table: Autor
+# Table: Autor
 key_autor = {}
-#(id nome)
+# (id nome)
 
-#Table: Genero
+# Table: Genero
 key_genero = {}
-#(id nome)
+# (id nome)
 
-#Table: MusicaAlbum
+# Table: MusicaAlbum
 musicaAlbum = {}
-#(idMusica, idAlbum)
+# (idMusica, idAlbum)
 
-#Table: autorAlbum
+# Table: autorAlbum
 autorAlbum = []
-#(idAutor, idAlbum)
+# (idAutor, idAlbum)
 
-#Table: PlaylistMusica
+# Table: PlaylistMusica
 playlistMusica = []
-#(idplaylist, idMusica)
 
+
+# (idplaylist, idMusica)
 
 def album_load(id):
     url = "https://api.deezer.com/album/" + str(id)
     response = urlopen(url)
     dj = json.loads(response.read())
-    
+
     id = dj["id"]
     nome = dj["title"]
     dataLancamento = dj["release_date"]
     nomeEditora = dj["label"]
-    
-    #criador de key para editora
+
+    # criador de key para editora
     if nomeEditora in editora_key:
         idEditora = editora_key[nomeEditora]
     else:
         editora_key[nomeEditora] = len(editora_key)
         idEditora = editora_key[nomeEditora]
-    
-    albums_list.append((id,nome,dataLancamento,idEditora))
-    
-    
-    
+
+    albums_list.append((id, nome, dataLancamento, idEditora))
+
     cover = dj["cover"]
-    
+
     genre_id = dj["genres"]["data"][0]["id"]
     genre_name = dj["genres"]["data"][0]["name"]
-    
+
     if genre_id not in key_genero:
         key_genero[genre_id] = genre_name
-    
-    
+
     artist_id = dj["artist"]["id"]
     artist_name = dj["artist"]["name"]
-    
+
     if artist_id not in key_autor:
         key_autor[artist_id] = artist_name
-    
-    autorAlbum.append((artist_id,id))
-    
-    
+
+    autorAlbum.append((artist_id, id))
+
     tracks = dj["tracks"]["data"]
-    tracks = list(map(lambda x:  (x["id"],x["title_short"]),tracks))
-    
+    tracks = list(map(lambda x: (x["id"], x["title_short"]), tracks))
+
     for track in tracks:
-        music_list.append((track[0],track[1],cover,random.randint(0, 10000000),"NONE"))
+        music_list.append((track[0], track[1], cover, random.randint(0, 10000000), ""))
         musicaAlbum[track[0]] = id
-    
+
+        print(music_list[-1])
 
 
 def playlist_creator():
-    for id_playlist in range(30): #creates 30 playlists
-        x = random.randint(5, 15) #playlist com 5 a 15 musicas
+    for id_playlist in range(30):  # creates 30 playlists
+        x = random.randint(5, 15)  # playlist com 5 a 15 musicas
         lista = random.sample(music_list, x)
         for musica in lista:
-            playlistMusica.append((id_playlist,musica[0]))
+            playlistMusica.append((id_playlist, musica[0]))
 
 
+if __name__ == '__main__':
+    # Daft Punk
+    album_load(302127)
+    album_load(6575789)
+    album_load(6703346)  # (feat. Pharrell Williams & Nile Rodgers)
+    album_load(301775)
 
+    # Arctic Monkeys
+    album_load(6899610)
+    album_load(401346)
+    album_load(401340)
+    album_load(401361)
+    # album_load(1166556)
+    # album_load(63203772)
+    # album_load(426670)
 
-#Daft Punk    
-album_load(302127)
-album_load(6575789)
-album_load(6703346) #(feat. Pharrell Williams & Nile Rodgers)
-album_load(301775)
+    # AlexTurner(membro Arctic Monkeys)
+    album_load(921000)
 
-#Arctic Monkeys
-album_load(6899610)
-album_load(401346)
-album_load(401340)
-album_load(401361)
-#album_load(1166556)
-#album_load(63203772)
-#album_load(426670)
+    # Tamino
+    album_load(75624372)
 
-#AlexTurner(membro Arctic Monkeys)
-album_load(921000)
+    # X-tense
+    album_load(106585062)
 
-#Tamino
-album_load(75624372)
+    # Djavan
+    album_load(7371939)
 
-#X-tense
-album_load(106585062)
+    # Joao Gilberto
+    album_load(228644)
 
-#Djavan
-album_load(7371939)
+    # Beatles
+    album_load(12047958)
+    album_load(12047952)
+    album_load(12047956)
 
-#Joao Gilberto
-album_load(228644)
+    # Kanye West
+    album_load(13357219)
+    album_load(116355212)
 
-#Beatles
-album_load(12047958)
-album_load(12047952)
-album_load(12047956)
+    # Chico da Tina
+    album_load(265953002)
+    album_load(214037042)  # (feat. eddy0)
 
-#Kanye West
-album_load(13357219)
-album_load(116355212)
+    # TylerTheCreator
+    album_load(44730061)
+    album_load(97140952)
 
-#Chico da Tina
-album_load(265953002)
-album_load(214037042) #(feat. eddy0)
+    playlist_creator()
 
-#TylerTheCreator
-album_load(44730061)
-album_load(97140952)
+    print(music_list)
 
-
-
-playlist_creator()
 
 
