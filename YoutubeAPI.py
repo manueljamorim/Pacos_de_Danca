4868117d44b3cb2f9ac06d391fd03365c2077154
Diogo Costa <diogocosta876@hotmail.com>
2021-12-08 13:32:15 +0000

    Added internal id searching

--- YoutubeAPI.py
+++ YoutubeAPI.py
@@ -7,16 +7,17 @@ DEVELOPER_KEY = "AIzaSyDSLv-JDuqmg22HQIClcGiEZkJsiW5rO6o"
 YOUTUBE_API_SERVICE_NAME = "youtube"
 YOUTUBE_API_VERSION = "v3"
 
-def isFoundInFile(titulo):
+def isFoundInFile(internal_id_search):
     file1 = open('MusicVideosInfo.txt', 'r')
     lines = file1.readlines()
     for line in lines:
-        title = line.split("///")[0]
-        if title == titulo:
+        internal_id = int(line.split("///")[0])
+        if internal_id == internal_id_search:
+            print("found in api")
             return True
     return False
 
-def youtube_search(titulo):
+def youtube_search(titulo, id_interno_musica = -1):
     youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
     developerKey=DEVELOPER_KEY)
 
@@ -35,27 +36,27 @@ def youtube_search(titulo):
     for search_result in search_response.get("items", []):
         if search_result["id"]["kind"] == "youtube#video":
             video_title = search_result["snippet"]["title"]
-            if isFoundInFile(video_title):
+            if isFoundInFile(id_interno_musica):
                 file1 = open('MusicVideosInfo.txt', 'r')
                 lines = file1.readlines()
                 for line in lines:
-                    title = line.split("///")[0]
-                    if title == video_title:
+                    internal_id = int(line.split("///")[0])
+                    if internal_id == id_interno_musica:
                         print("returned from file")
                         return line
             else:
                 video_id = search_result["id"]["videoId"]
                 stats = youtube.videos().list(part='statistics', id=video_id).execute()
                 file1 = open('MusicVideosInfo.txt', 'a')
-                file1.write("%s///%s///%s\n" % (video_title, video_id, stats["items"][0]["statistics"]["viewCount"]))
-                video_info.append("%s///%s///%s" % (video_title, video_id, stats["items"][0]["statistics"]["viewCount"]))
+                file1.write("%s///%s///%s///%s\n" % (id_interno_musica ,video_title, video_id, stats["items"][0]["statistics"]["viewCount"]))
+                video_info.append("%s///%s///%s///%s\n" % (id_interno_musica ,video_title, video_id, stats["items"][0]["statistics"]["viewCount"]))
                 print("returned from api")
-                return video_info
+                return video_info[0]
     return "-"
 
 #to test with an example
-print(youtube_search("chico da tina"))
-print(youtube_search("Highway To Hell"))
+print(youtube_search("chico da tina", 5))
+print(youtube_search("Highway To Hell", 34))
     
     
 
